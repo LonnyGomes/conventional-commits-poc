542dd6f0ae53f1262c05cc08c017ba528b8dee53
Lonny Gomes <code@lonnygomes.com>
2019-10-06 19:37:39 -0400

    feat: add @semantic-release/git dependency

--- release.config.js
+++ release.config.js
@@ -1,7 +1,26 @@
 module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator'
+        {
+            preset: 'angular',
+            releaseRules: [
+                { type: 'docs', scope: 'README', release: 'patch' },
+                { type: 'refactor', release: 'patch' },
+                { type: 'style', release: 'patch' }
+            ],
+            parserOpts: {
+                noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES']
+            }
+        },
+        '@semantic-release/release-notes-generator',
+        {
+            preset: 'angular',
+            writerOpts: {
+                commitsSort: ['subject', 'scope']
+            }
+        },
+        '@semantic-release/changelog',
+        '@semantic-release/git'
     ],
     ci: false
 };
